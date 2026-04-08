1da589b41a42506024c62646ea718f885bab5a3e
ok797738 <ok797738@host.docker.internal>
2026-04-08 16:30:35 -0500

    left panel now is a separate file, js moved to script file, make post method checks if post has image or not, bug with comments container fixed, some css added.

--- src/java/controllers/MemberController.java
+++ src/java/controllers/MemberController.java
@@ -161,10 +161,18 @@ public class MemberController extends HttpServlet {
                 } catch (ServletException ex) {
                     System.out.println("Issue with Servlet file parts -> \nError thrown:" + ex);
                 }
-
-                int postId = PostDB.makePost(userID, imageURL, postText);
+                
+                String test = username + "/upload/";
+                int postId = -1;
                 boolean success = false;
-                if (postId != -1) {
+                if(!test.equals(imageURL) ){
+                    postId= PostDB.makePost(userID, imageURL, postText);
+                }
+                else{
+                    postId= PostDB.makePost(userID, null, postText);
+                    success = true;
+                }
+                if (postId != -1 && !test.equals(imageURL)) {
                     success = PostDB.createPostImage(imageURL, postId, userID);
                 }
                 if (success) {
---
+++ web/member/script.js
@@ -0,0 +1,49 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ */
+
+
+const profileImg = document.querySelector('#profilePicShape'); // or '.hover'
+const textElem = document.querySelector('#changeImage');
+const uploadForm = document.querySelector('.uplaod_image_form');
+const closeUploadForm = document.querySelector("#close_uplaod_image_form");
+const commentsBtn = document.querySelectorAll(".commentsBtn");
+const closeCommentsBtn = document.querySelectorAll("#close_comments");
+const status = document.querySelector(".status");
+const statusForm = document.querySelector(".status_form");
+
+status?.addEventListener('click', () => {
+    status.classList.add('hidden');
+    statusForm.classList.remove('hidden');
+
+
+});
+
+commentsBtn.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        id = e.target.id.replace('commentsBtn-', '');
+        commentsContainer = document.querySelector(`#commentsContainer-${id}`);
+        commentsContainer.classList.remove('hidden');
+    });
+});
+closeCommentsBtn.forEach(btn => {
+    btn?.addEventListener('click', () => {
+        commentsContainer = document.querySelectorAll('.comments_container');
+        commentsContainer.forEach(cont => cont.classList.add('hidden'));
+    });
+});
+
+profileImg?.addEventListener('click', () => {
+    console.log('Profile photo clicked!');
+    uploadForm.classList.remove('hidden');
+    //window.location.href = "https://www.gfg.com/";
+    //                    document.location.href = "${pageContext.request.contextPath}/Member?action=uploadProfilePhoto"; //https://coderanch.com/t/351229/java/calling-servlet-javascript-function
+
+    //https://www.geeksforgeeks.org/html/how-to-get-browser-to-navigate-url-in-javascript/
+    //https://www.w3schools.com/js/js_events_mouse.asp
+});
+
+closeUploadForm?.addEventListener('click', () => {
+    uploadForm.classList.add('hidden');
+});
